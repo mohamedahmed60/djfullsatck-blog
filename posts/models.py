ba91113b64ffe1debbee54509ac3decedb900dd1
mohamedahmed60 <mohamedaljabri6666@gmail.com>
2022-12-25 22:21:34 +0300

    add category

--- posts/models.py
+++ posts/models.py
@@ -8,7 +8,15 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=1000)
     image = models.ImageField(upload_to='posts/')
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='post_category')
 
     def __str__(self) -> str: # كيف يمكن ارجاع القيمة لك
         return f"{self.title }-{str(self.author)}"  # لعرض العنون مع المستخدم الذي قام بادخاله
 
+# علاقة  category و  post العلاقة بين ال
+# one to many
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self) -> str:
+        return self.name.__str__()
