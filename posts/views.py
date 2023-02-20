268a0dadce5e86ec05a8e5765f970185bedbc307
mohamedahmed60 <mohamedaljabri6666@gmail.com>
2023-02-20 10:25:25 +0300

    edit the scrollbar

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render , redirect
 from .models import Post , About
 from .forms import PostForm
@@ -6,7 +7,10 @@ from .forms import PostForm
 
 def all_posts(request):
     posts = Post.objects.all()         #''' select * from posts '''
-    return render(request,'blog/post_list.html',{'object':posts})
+    paginator = Paginator(posts, 4)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request,'blog/post_list.html',{'object':page_obj})
     # return render(request,'posts.html',{'mahmoud':posts})
 
 
